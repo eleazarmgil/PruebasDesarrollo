7077421c2d78087e7fea97a716d3b6d301b6759c
Maria Vasquez <mgvasquez.19@est.ucab.edu.ve>
2023-05-19 19:03:21 -0400

    agregando archivos faltantes

---
+++ src/pagalotodo-ucab-ms/UCABPagaloTodoMS.Infrastructure/services/Rabbit.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UCABPagaloTodoMS.Infrastructure.Services
+{
+    public class Rabbit
+    {
+        public void SendProductMessage<T>(T message) 
+        {
+            try
+            {
+                var factory = new ConnectionFactory
+                {
+                    HostName = "localhost",
+                };
+
+                var connection = factory.CreateConnection();
+
+                using var channel = connection.CreateModel();
+
+                channel.QueueDeclare("casita", exclusive: false);
+
+                var json = JsonConvert.SerializeObject(message);
+
+                var body = Encoding.UTF8.GetBytes(json);
+
+                channel.BasicPublish(exchange: "amq.topic", routingKey: "product", body: body);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+
+    }
+
+}
