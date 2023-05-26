e389514fce03538ca0321063b1946bb8769b71bc
Maria Vasquez <mgvasquez.19@est.ucab.edu.ve>
2023-05-25 20:45:57 -0400

    agregando buscar servicio por id_prestador

--- src/pagalotodo-ucab-ms/UCABPagaloTodoMS.Application/Requests/ConsultarServicoEmpresaRequest.cs
+++ src/pagalotodo-ucab-ms/UCABPagaloTodoMS.Application/Requests/ConsultarServicoEmpresaRequest.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace UCABPagaloTodoMS.Application.Requests
+﻿namespace UCABPagaloTodoMS.Application.Requests;
+public class ConsultarServicioEmpresaRequest
 {
-    internal class ConsultarServicoEmpresa
-    {
-    }
+    public Guid id_prestador { set; get; }
+    
 }
