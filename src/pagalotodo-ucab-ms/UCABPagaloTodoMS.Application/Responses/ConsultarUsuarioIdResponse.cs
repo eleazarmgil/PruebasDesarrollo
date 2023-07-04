5befedb0be47db1a0de8dc480edcb9acddb87948
Maria Vasquez <mgvasquez.19@est.ucab.edu.ve>
2023-07-03 22:43:40 -0400

    Agregando response de usuario por id

---
+++ src/pagalotodo-ucab-ms/UCABPagaloTodoMS.Application/Responses/ConsultarUsuarioIdResponse.cs
@@ -0,0 +1,17 @@
+﻿
+namespace UCABPagaloTodoMS.Application.Responses;
+
+public class ConsultarUsuarioIdResponse
+{
+    public Guid id_usuario { get; set; }
+    public string? usuario { get; set; }
+    public string? ci { get; set; }
+    public string? rif { get; set; }
+    public string? nombre_empresa { get; set; }
+    public string? correo { get; set; }
+    public string? nombre { get; set; }
+
+    public bool? estado { set; get; }
+
+    public string? Discriminator { set; get; }
+}
